8a147fed97069d61747635fedbbc782b6acc0ebb
Andrew Barnett <Abarn279@gmail.com>
2017-12-04 12:05:01 -0600

    code golfed it!

--- src/__4a.py
+++ src/__4a.py
@@ -2,16 +2,4 @@ from file_importer import FileImporter
 
 inp = [i.split(" ") for i in FileImporter.get_input("/../input/4.txt").split("\n")]
 
-count = 0
-for line in inp:
-    lset = set()
-    allUnique = True
-    for word in line:
-        if word in lset:
-            allUnique = False
-            break
-        lset.add(word)
-    if allUnique:
-        count += 1
-print(count)    
-
+print(len([l for l in inp if len(set(l)) == len(l)]))
