9473ef51ac1f1be410b7be7f4eac021571ace102
Andrew Barnett <Abarn279@gmail.com>
2017-12-04 12:08:48 -0600

    gode golfed it!

--- src/__4b.py
+++ src/__4b.py
@@ -2,5 +2,4 @@ from file_importer import FileImporter
 
 inp = [i.split(" ") for i in FileImporter.get_input("/../input/4.txt").split("\n")]
 
-print(len([l for l in inp if len(set(["".join(sorted(i)) for i in l])) == len(["".join(sorted(i)) for i in l])]))    
-
+print(len([l for l in inp if len(set(["".join(sorted(i)) for i in l])) == len(["".join(sorted(i)) for i in l])]))
