39b109ef80406bc40b7bac8e402fb30339ac6288
Andrew Barnett <Abarn279@gmail.com>
2017-12-06 09:56:20 -0600

    more try-catch for code flow!

---
+++ src/__6a.py
@@ -0,0 +1,29 @@
+from file_importer import FileImporter
+
+inp = list(map(int, FileImporter.get_input("/../input/6.txt").split()))
+
+def max_bank_ind(banks):
+    max = 0
+    for i in range(len(banks)):
+        if banks[i] > banks[max]:
+            max = i
+    return max
+
+def realloc(banks, bankInd):
+    blocks, banks[bankInd] = banks[bankInd], 0
+    while blocks > 0:
+        bankInd += 1
+        banks[bankInd % len(banks)] += 1
+        blocks -= 1
+
+states, count = [], 0
+while True:
+    if inp in states:
+        break
+
+    states.append(inp[:])
+    maxInd = max_bank_ind(inp)
+    realloc(inp, maxInd)
+    count += 1
+
+print(count)
---
+++ src/__6b.py
@@ -0,0 +1,33 @@
+from file_importer import FileImporter
+
+inp = list(map(int, FileImporter.get_input("/../input/6.txt").split()))
+
+def max_bank_ind(banks):
+    max = 0
+    for i in range(len(banks)):
+        if banks[i] > banks[max]:
+            max = i
+    return max
+
+def realloc(banks, bankInd):
+    blocks, banks[bankInd] = banks[bankInd], 0
+    while blocks > 0:
+        bankInd += 1
+        banks[bankInd % len(banks)] += 1
+        blocks -= 1
+
+states, count = [], 0
+seenTwice = []
+while True:
+    if inp in states:
+        try: 
+            ind = [i[0] for i in seenTwice].index(inp)
+            print(count - seenTwice[ind][1])
+            break
+        except:
+            seenTwice.append((inp[:], count))
+
+    states.append(inp[:])
+    maxInd = max_bank_ind(inp)
+    realloc(inp, maxInd)
+    count += 1
