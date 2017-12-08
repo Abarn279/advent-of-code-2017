0edaccc03f7d9e1b97d95f923e752e7e56223268
Andrew Barnett <Abarn279@gmail.com>
2017-12-08 11:30:15 -0600

    too many lemons to squeeze

---
+++ src/__8a.py
@@ -0,0 +1,13 @@
+from file_importer import FileImporter
+from collections import defaultdict
+import operator
+ops = { '+': operator.add, '>': operator.gt, '<': operator.lt, '>=': operator.ge, '<=': operator.le, '==': operator.eq, '!=': operator.ne, 'inc': operator.add, 'dec': operator.sub }
+
+inp = [i.split() for i in FileImporter.get_input("/../input/8.txt").split("\n")]
+registers = defaultdict(lambda: 0)
+
+for i in inp:
+    reg, op, val, condReg, condOp, condVal = i[:3] + i[4:]  # Unpack input, ignore 'if'
+    if not ops[condOp](registers[condReg], int(condVal)): continue
+    registers[reg] = ops[op](registers[reg], int(val))
+print(max(registers.values()))
