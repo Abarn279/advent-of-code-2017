9cabcacc52594d8813eca1b625cea3fe092eb943
Andrew Barnett <Abarn279@gmail.com>
2017-12-09 10:23:36 -0600

    no amount of whitespace can fix this

--- src/__9a.py
+++ src/__9a.py
@@ -9,7 +9,6 @@ while i < len(inp):
     if inp[i] == '!':
         i += 2
         continue
-
     if inp[i] == '<':
         garbage = True
         i += 1; continue
@@ -18,14 +17,11 @@ while i < len(inp):
         i += 1; continue
     if garbage:
         i += 1; garbageChars += 1; continue
-
-    
     elif inp[i] == '{':
         scoreToAdd += 1
     elif inp[i] == '}':
         score += scoreToAdd
         scoreToAdd -= 1
-
     i += 1
 
 print(score)
--- src/__9b.py
+++ src/__9b.py
@@ -8,23 +8,19 @@ while i < len(inp):
     if inp[i] == '!':
         i += 2
         continue
-
     if inp[i] == '<' and not garbage:
         garbage = True
         i += 1; continue
     elif inp[i] == '>' and garbage:
         garbage = False
         i += 1; continue
-        
     if garbage:
         i += 1; garbageChars += 1; continue
-    
     elif inp[i] == '{':
         scoreToAdd += 1
     elif inp[i] == '}':
         score += scoreToAdd
         scoreToAdd -= 1
-
     i += 1
 
 print(garbageChars)
