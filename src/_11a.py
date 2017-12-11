d3580e2ec63e86fe5c0efdfc50a7b473cce7e717
Andrew Barnett <Abarn279@gmail.com>
2017-12-11 13:33:54 -0600

    abs not fabs

--- src/_11a.py
+++ src/_11a.py
@@ -1,6 +1,5 @@
 from file_importer import FileImporter
 from collections import defaultdict
-from math import fabs
 
 inp = FileImporter.get_input("/../input/11.txt").split(',')
 dic = defaultdict(lambda: 0)
@@ -8,8 +7,8 @@ dic = defaultdict(lambda: 0)
 for i in inp:
     dic[i] += 1
 
-xabs = fabs((dic["ne"] + dic["se"]) - (dic["nw"] + dic["sw"]))
-yabs = fabs((dic["n"] + dic["nw"]) - (dic["s"] + dic["se"]))
-zabs = fabs((dic["s"] + dic["sw"]) - (dic["n"] + dic["ne"]))
+xabs = abs((dic["ne"] + dic["se"]) - (dic["nw"] + dic["sw"]))
+yabs = abs((dic["n"] + dic["nw"]) - (dic["s"] + dic["se"]))
+zabs = abs((dic["s"] + dic["sw"]) - (dic["n"] + dic["ne"]))
 
 print(int((xabs + zabs + yabs) / 2))
--- src/_11b.py
+++ src/_11b.py
@@ -1,6 +1,5 @@
 from file_importer import FileImporter
 from collections import defaultdict
-from math import fabs
 
 inp = FileImporter.get_input("/../input/11.txt").split(',')
 dic = defaultdict(lambda: 0)
@@ -8,9 +7,9 @@ dic = defaultdict(lambda: 0)
 max = 0
 for i in inp:
     dic[i] += 1
-    xabs = fabs((dic["ne"] + dic["se"]) - (dic["nw"] + dic["sw"]))
-    yabs = fabs((dic["n"] + dic["nw"]) - (dic["s"] + dic["se"]))
-    zabs = fabs((dic["s"] + dic["sw"]) - (dic["n"] + dic["ne"]))
+    xabs = abs((dic["ne"] + dic["se"]) - (dic["nw"] + dic["sw"]))
+    yabs = abs((dic["n"] + dic["nw"]) - (dic["s"] + dic["se"]))
+    zabs = abs((dic["s"] + dic["sw"]) - (dic["n"] + dic["ne"]))
     dist = int((xabs + zabs + yabs) / 2)
     if dist > max:
         max = dist
