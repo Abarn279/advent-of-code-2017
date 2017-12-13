bfaac666c14becd73930467e67ffa8ae82a77f17
Andrew Barnett <Abarn279@gmail.com>
2017-12-13 12:23:29 -0600

    13b done

---
+++ src/_13b.py
@@ -0,0 +1,39 @@
+from file_importer import FileImporter
+from enum import Enum
+
+inp = [list(map(int, i.split(": "))) for i in FileImporter.get_input("/../input/13.txt").split('\n')]
+
+class Scanner:
+    def __init__(self, rang):
+        self.rang = rang
+
+    def get_pos(self, ps):
+        indRang = self.rang - 1
+        return abs(indRang - ((ps + indRang) % (indRang*2)))
+
+def part2(inp):
+    delay = 0
+    while True:
+        scanners = [None for x in range(inp[-1][0] + 1)]                          # List of scanner objects
+        for i in inp:
+            scanners[i[0]] = Scanner(i[1])                                        # Create a scanner
+
+        step = 0
+        found = False
+        for i in scanners:
+            if i is None:
+                step += 1 
+                continue
+
+            pos = i.get_pos(step + delay)
+            if pos == 0:
+                found = True
+                break
+            step += 1
+
+        if not found:
+            return delay
+
+        delay += 1
+
+print(part2(inp))
